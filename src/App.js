d730bdd936495a5c57ee649d600c5e4ac5715741
SamuelDiaz2 <samuel.diazf@uniagustiniana.edu.co>
2025-10-01 15:49:07 +0000

    Calculadora componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Contador from './Contador';
+import Button from './Boton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Contador />
+        <Button/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
