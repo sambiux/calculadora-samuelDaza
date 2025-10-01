7b52a5752bcf2f4c5957109b768967e13fd57b9d
SamuelDiaz2 <samuel.diazf@uniagustiniana.edu.co>
2025-10-01 17:06:48 +0000

    calculadora logica

--- src/Contador.js
+++ src/Contador.js
@@ -1,26 +1,29 @@
 import { useState } from "react";
 
 function Contador() {
-  const [contador, setContador] = useState(0);  
-    const incrementar = () => setContador(contador + 1);
-    const InputNumber = (num) => setContador(num);
+    const [contador, setContador] = useState("");
+    const incrementar = () => setContador((parseInt(contador) || 0) + 1 + "");
+    const agregarNumero = (num) => setContador(contador + num);
 
+
+    
     return (
         <div id ="contador">
             <label>Contador: {contador}</label>
             <button onClick={incrementar}>Incrementar</button>
-            <button onClick={() =>InputNumber(1)}>1</button>
-            <button onClick={() =>InputNumber(2)}>2</button>
-            <button onClick={() =>InputNumber(3)}>3</button>
-            <button onClick={() =>InputNumber(4)}>4</button>
-            <button onClick={() =>InputNumber(5)}>5</button>
-            <button onClick={() =>InputNumber(6)}>6</button>
-            <button onClick={() =>InputNumber(7)}>7</button>
-            <button onClick={() =>InputNumber(8)}>8</button>
-            <button onClick={() =>InputNumber(9)}>9</button>
-            <button onClick={() =>InputNumber(0)}>0</button>
-            
-            
+            <button onClick={() => setContador("")}>Reset</button>
+            <button onClick={() =>agregarNumero("1")}>1</button>
+            <button onClick={() =>agregarNumero("2")}>2</button>
+            <button onClick={() =>agregarNumero("3")}>3</button>
+            <button onClick={() =>agregarNumero("4")}>4</button>
+            <button onClick={() =>agregarNumero("5")}>5</button>
+            <button onClick={() =>agregarNumero("6")}>6</button>
+            <button onClick={() =>agregarNumero("7")}>7</button>
+            <button onClick={() =>agregarNumero("8")}>8</button>
+            <button onClick={() =>agregarNumero("9")}>9</button>
+            <button onClick={() =>agregarNumero("0")}>0</button>
+            <button onClick={() =>agregarNumero("+")}>+</button>
+            <button onClick={() =>agregarNumero("-")}>-</button>
         </div>
     );
 }
